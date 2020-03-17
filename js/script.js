ddffd73313ff1f6dfbe9ebbbcce4cae74cbb8919
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2020-03-17 09:00:52 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,65 +1,4 @@
-
-
-// create an array with happy tree friends
-
-
-// duplicate the content of this array
-
-
-// shuffle the array
-
- 
-// initialize all blocks, put them in an array
-
-
-// initialize url params
-
-
-// initialize an empty array that will contain the active blocks
-
-
-//Function: helper to "pause" the game after 2 active blocks
-// loop through each block to add functionality
-  
-  // combine url
-
-  // add the background image to each block + add a class
-
-  
-
-
-// functionality after a click
-    // if less thean 2 blocks, make block visible
-
-    // if 2 blocks are active, check them and hide or remove them
-      // pause parameter
-        // pause while checking
-
-        // automatically hide the blocks after 2 seconds
-
-
-
-
-//Function: show block, change opacity and add to activeblock-array
-
-//Function: Check if block is the same as active block
-
-//Function: Remove found blocks, change backgroundimage to 'memory-found
-
-//Function: hide blocks, change opacity again
-
-
-//Function addElement 
-  // create a new div element 
-  
-  // add a class to the newly created element
-  
-  // add the background image to the element
-  
-  // add the newly created element and its content into the DOM 
-
-
-
+// ALL FUNCTIONS
 /**
  * Shuffles array in place.
  * @param {Array} a items The array containing the items.
@@ -75,4 +14,83 @@ function shuffle(a) {
         a[i - 1] = a[j];
         a[j] = x;
     }
+}
+
+//Helper to "pause" the game after 2 active blocks
+var holdOn = false;
+
+
+
+//Function: show block, change opacity and add to activeblock-array
+function showBlock(block) {
+
+  }
+
+//Function: Check if block is the same as active block
+function checkBlocks() {
+
+  }
+
+//Function: Remove found blocks, change backgroundimage to 'memory-found
+function removeBlocks() {
+
+  }
+
+//Function: hide blocks, change opacity again
+function hideBlocks() {
+
+  }
+
+
+//Function addElement 
+function addElement (background) { 
+     // create a new div element 
+  
+    // add a class to the newly created element
+    
+    // add the background image to the element
+    
+    // add the newly created element and its content into the DOM 
+  }
+
+
+
+
+
+// create an array with happy tree friends
+var happytreefriends = [];
+
+// duplicate the content of this array: use concat
+
+
+// shuffle the array 
+shuffle(doublefriends);
+ 
+// initialize all blocks, put them in an array: DOM, get element by id: 'memory-block'
+var blocks;
+
+// initialize url params
+var urlPrefix = "img/";
+var urlSuffix = ".png";
+
+
+
+// initialize an empty array that will contain the active blocks
+for(var i = 0; i < blocks.length; i++) {
+// loop through each block to add functionality
+  // combine url
+  // add the background image to each block + add a class
+
+
+    // functionality after a click
+    blocks[i].addEventListener('click', function(event) {
+        // if less thean 2 blocks, make block visible
+
+        // if 2 blocks are active, check them and hide or remove them
+        // pause parameter
+            // pause while checking
+
+            // automatically hide the blocks after 2 seconds
+
+    });
 }
